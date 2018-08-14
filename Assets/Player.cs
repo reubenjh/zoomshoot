4dd7dee42702115024448fd276019c217ef9c4d9
reubenjh <rjharcourt@gmail.com>
2018-08-15 04:22:41 +1200

    camera following rocket - horizontal x speed calculating from horizontal throw and mps

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+	[SerializeField] float xMps = 1f;
+	private float xMove;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		xMove = horizontalThrow * xMps;
+		print(xMove);
+	}
+}
