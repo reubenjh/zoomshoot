e6844fcf0416f2614c92a72e311f19dc563bf855
reubenjh <rjharcourt@gmail.com>
2018-08-15 02:37:50 +1200

    made splash scene, music player, script to make scenes auto transitiona dn music continuously play

---
+++ Assets/musicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicPlayer : MonoBehaviour {
+
+	private void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke("loadFirstScene", 5f);
+
+	}
+
+	void loadFirstScene() {
+		SceneManager.LoadScene(1);
+	}
+}
